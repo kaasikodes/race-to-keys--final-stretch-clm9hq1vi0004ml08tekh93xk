d7a5f37685093d9532ca613aaf6333e15b6f62f8
kaasikodes <thisbroisfresh@gmail.com>
2023-11-11 14:12:28 +0100

    fix: ensures auth user does not have access to auth page

--- app/components/modules/auth/LoginContainer.tsx
+++ app/components/modules/auth/LoginContainer.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { useSession, signIn } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../../ui/button";
 import { FaTwitter } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const LoginContainer = () => {
+  const { data: session } = useSession();
+  const { push } = useRouter();
+  useEffect(() => {
+    if (!session) {
+      push("/dashboard");
+    }
+  }, [push, session]);
   return (
     <div className="w-full h-full flex flex-col gap-4 items-center justify-center text-white text-center -mt-20">
       <h4 className="text-2xl font-bold">Welcome to Sofii</h4>
