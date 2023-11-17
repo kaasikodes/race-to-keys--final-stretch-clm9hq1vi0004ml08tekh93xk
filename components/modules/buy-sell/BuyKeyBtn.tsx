8f519bc06f196980d1a3f0a0555e1f99abcce86c
kaasikodes <thisbroisfresh@gmail.com>
2023-11-17 07:19:54 +0100

    chore: ensures buy/sell fn

--- components/modules/buy-sell/BuyKeyBtn.tsx
+++ components/modules/buy-sell/BuyKeyBtn.tsx
@@ -57,7 +57,7 @@ const BuyKeyBtn: React.FC<{
       <BuyOrSellKeys
         onClose={handleClose}
         open={view}
-        keySubjectAddress={view !== undefined ? undefined : keySubjectAddress}
+        keySubjectAddress={view === undefined ? undefined : keySubjectAddress}
         transactionType={"buy"}
         onConfirm={{ fn: (amount) => handleSubmit({ amount }), isLoading }}
       />
