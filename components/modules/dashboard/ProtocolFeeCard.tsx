507028694d141b56e6fa0b3eda874b0863b10621
kaasikodes <thisbroisfresh@gmail.com>
2023-11-17 06:00:57 +0100

    fix: ensures invisible text is visible

--- components/modules/dashboard/ProtocolFeeCard.tsx
+++ components/modules/dashboard/ProtocolFeeCard.tsx
@@ -5,8 +5,8 @@ const ProtocolFeeCard: React.FC<{ title?: string; text?: string }> = ({
   text,
 }) => {
   return (
-    <div className="text-black  border rounded-lg text-sm shadow px-5 pt-4 pb-6 flex-1">
-      <span className="text-caramel font-semibold text-lg text-blue-500">
+    <div className="text-black border rounded-lg text-sm shadow px-5 pt-4 pb-6 flex-1">
+      <span className="text-blackfont-semibold text-lg text-blue-500">
         {title}
       </span>
       <h6 className="text-xs font-semibold">{text}</h6>
