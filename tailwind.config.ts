29f26eaf728372b66792955f77f79e6297f67c23
kaasikodes <thisbroisfresh@gmail.com>
2023-11-17 06:13:37 +0100

    chore: adjusts tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      caramel: "var(--caramel)",
-      card: "var(--card)",
-      accent: "var(--accent)",
-      neutral: "var(--neutral)",
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        caramel: "var(--caramel)",
+        card: "var(--card)",
+        accent: "var(--accent)",
+        neutral: "var(--neutral)",
       },
     },
   },
